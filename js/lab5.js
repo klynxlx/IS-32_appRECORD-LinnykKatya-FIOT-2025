a818377735d75a31349c332c66d1b674e06c8a52
klynxlx <katunka.ua@gmail.com>
2025-11-11 19:58:19 +0200

    lab5 6

---
+++ js/lab5.js
@@ -0,0 +1,92 @@
+function showDetails5() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ОСНОВИ JAVASCRIPT У HTML-ДОКУМЕНТІ. ШАБЛОНІ РЯДКИ. КЕРУВАННЯ ПОРЯДКОМ ОБЧИСЛЕНЬ. МАСИВИ. МЕТОДИ МАСИВІВ. ФУНКЦІЇ</p>
+<p><strong>Мета:</strong> ⎯	придбати практичні навички роботи з конструкціями мови JS, масивами та фугкціями у js-сценаріях.  Реалізація програм засовами мови JAVASCRIPT</p>
+<strong>Місце розташування:</strong> <a href="https://github.com/klynxlx/IS-32_appWEB-LinnykKatya-FIOT-2025-" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://klynxlx.github.io/IS-32_appWEB-LinnykKatya-FIOT-2025-/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="https://github.com/klynxlx/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025" target="_blank">посилання на репозиторій самостійної роботи</a><br>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку самостійної роботи</a>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>`
+;}
+
+function Task1() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/task1.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/photo1.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/task2.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/photo2.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task3() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+                <img src="./images/task3.jpg" alt="Placeholder Image" width="700">
+                <img src="./images/photo3.jpg" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        function Task4() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./images/task4.jpg" alt="Placeholder Image" width="700">
+                   `
+                ;
+            }
+            function Task5() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./images/task5.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./images/photo5.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+
+                 function Task6() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./images/task6.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./images/photo6.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                   function Task7() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./images/task7.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./images/photo7.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                    function Task8() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./images/task8.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./images/photo8.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                    function Task9() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./images/task9.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./images/photo9.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+
+            function showConclusion5() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №5 я придбала практичні навички роботи з конструкціями мови JS, масивами та функціями у js-сценаріях. Також реалізувала програми засовами мови JAVASCRIPT</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }      
---
+++ js/lab6.js
@@ -0,0 +1,68 @@
+function showDetails6() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ОБ'ЄКТ. МЕТОДИ ОБ'ЄКТА. МАСИВ ОБ'ЄКТІВ. ДЕСТРУКТУРИЗАЦІЯ ОБ'ЄКТІВ. CALLBACK. СТРІЛОЧНІ ФУНКЦІЇ. СТРІЛОЧНІ ФУНКЦІЇ ЯК КОЛБЕКИ.</p>
+<p><strong>Мета:</strong> придбати практичні навички роботи з об'єктами. Методи об'єкта.. Callback. Стрілочні функції. Стрілочні функції як колбеки.
+.  Реалізація програм засовами мови JAVASCRIPT</p>
+<strong>Місце розташування:</strong> <a href="https://github.com/klynxlx/IS-32_appWEB-LinnykKatya-FIOT-2025-" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://klynxlx.github.io/IS-32_appWEB-LinnykKatya-FIOT-2025-/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="https://github.com/klynxlx/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025" target="_blank">посилання на репозиторій самостійної роботи</a><br>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку самостійної роботи</a>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>`
+;}
+
+function Task2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/taask2.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/taask22.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task4() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/taask4.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/taask44.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task6() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+                <img src="./images/taask6.jpg" alt="Placeholder Image" width="700">
+                <img src="./images/taask66.jpg" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        function Task8() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+                    <img src="./images/taask8.jpg" alt="Placeholder Image" width="700">
+                    <img src="./images/taask8.1.jpg" alt="Placeholder Image" width="700">
+                    <img src="./images/task88.jpg" alt="Placeholder Image" width="700">`
+                ;
+            }
+            function Task9() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                        <img src="./images/taask9.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./images/taask99.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                function Task10() {
+                    var outputDiv = document.getElementById("output");
+                    outputDiv.innerHTML = `
+                            <img src="./images/taask10.jpg" alt="Placeholder Image" width="700">
+                        
+                            <img src="./images/task10.jpg" alt="Placeholder Image" width="700">`
+                        ;
+                    }
+            function showConclusion6() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №5 я придбала практичні навички роботи з об'єктами. Методи об'єкта.. Callback. Стрілочні функції. Стрілочні функції як колбеки.  Реалізація програм засовами мови JAVASCRIPT</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }  

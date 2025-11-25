4211fafb9cdb796b548ff5f6182122b49254ca16
klynxlx <katunka.ua@gmail.com>
2025-11-26 00:04:16 +0200

    update

---
+++ js/lab7.js
@@ -0,0 +1,84 @@
+function showDetails7() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> ПРОТОТИПИ. КЛАСИ. ОБ'ЄКТНА МОДЕЛЬ ДОКУМЕНТА (DOM). ПОДІЇ. ОБ'ЄКТ ПОДІЇ. ДЕЛЕГУВАННЯ ПОДІЙ. </p>
+<p><strong>Мета:</strong> придбати практичні навички роботи з DOM, подіями, делегуванням подій</p>
+<strong>Місце розташування:</strong> <a href="https://github.com/klynxlx/IS-32_appWEB-LinnykKatya-FIOT-2025-" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://klynxlx.github.io/IS-32_appWEB-LinnykKatya-FIOT-2025-/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="https://github.com/klynxlx/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025" target="_blank">посилання на репозиторій самостійної роботи</a><br>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку самостійної роботи</a>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>`
+;}
+
+function Task7_2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+       <li><a href="./lab7.tasks/task7_2.html">Завдання 2</a></li>
+            <img src="./lab7.tasks/images7/task2.jpg" alt="Placeholder Image" width="700">
+            <img src="./lab7.tasks/images7/taask2.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task7_4() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+    <li><a href="./lab7.tasks/task7_4.html">Завдання 4</a></li>
+            <img src="./lab7.tasks/images7/task4.jpg" alt="Placeholder Image" width="700">
+            <img src="./lab7.tasks/images7/taask4.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+    function Task7_6() {
+        var outputDiv = document.getElementById("output");
+        outputDiv.innerHTML = `
+        <li><a href="./lab7.tasks/task7_6.html">Завдання 6</a></li>
+                <img src="./lab7.tasks/images7/task6.jpg" alt="Placeholder Image" width="700">
+                <img src="./lab7.tasks/images7/taask6.jpg" alt="Placeholder Image" width="700">`
+                
+            ;
+        }
+        function Task7_7() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+            <li><a href="./lab7.tasks/task7_7.html">Завдання 7</a></li>
+                    <img src="./lab7.tasks/images7/task7.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab7.tasks/images7/taask7.jpg" alt="Placeholder Image" width="700">
+                   `
+                ;
+            }
+        function Task7_8() {
+            var outputDiv = document.getElementById("output");
+            outputDiv.innerHTML = `
+            <li><a href="./lab7.tasks/task7_8.html">Завдання 8</a></li>
+                    <img src="./lab7.tasks/images7/task8_2.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab7.tasks/images7/task8_1.jpg" alt="Placeholder Image" width="700">
+                    <img src="./lab7.tasks/images7/taask8.jpg" alt="Placeholder Image" width="700">`
+                ;
+            }
+            function Task7_9() {
+                var outputDiv = document.getElementById("output");
+                outputDiv.innerHTML = `
+                <li><a href="./lab7.tasks/task7_9.html">Завдання 9</a></li>
+                        <img src="./lab7.tasks/images7/task9.jpg" alt="Placeholder Image" width="700">
+                    
+                        <img src="./lab7.tasks/images7/taask9.jpg" alt="Placeholder Image" width="700">`
+                    ;
+                }
+                function Task7_10() {
+                    var outputDiv = document.getElementById("output");
+                    outputDiv.innerHTML = `
+                    <li><a href="./lab7.tasks/task7_10.html">Завдання 10</a></li>
+                            <img src="./lab7.tasks/images7/task10.jpg" alt="Placeholder Image" width="700">
+                        
+                            <img src="./lab7.tasks/images7/taask10.jpg" alt="Placeholder Image" width="700">`
+                        ;
+                    }
+            function showConclusion7() {
+                var outputDiv = document.getElementById("output");
+                var conclusion = `
+                    <p><strong>Висновок:</strong> Під час виконання ЛР №6 я придбала практичні навички роботи з об'єктами. Методи об'єкта.. Callback. Стрілочні функції. Стрілочні функції як колбеки.
+.  Реалізація програм засовами мови JAVASCRIPT
+</p>`
+                ;
+                outputDiv.innerHTML = conclusion;
+            }  
---
+++ js/lab8.js
@@ -0,0 +1,39 @@
+function showDetails8() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+<p><strong>Тема:</strong> WEB-СХОВИЩЕ. WEB STORAGE API. МОДУЛЬНОСТЬ  КОДУ. СТВОРЕННЯ МОДАЛЬНОГО ВІКНА. ДЕЛЕГУВАННЯ ПОДІЙ. </p>
+<p><strong>Мета:</strong> придбати практичні навички роботи створення модального вікна; повторити процес створення макету, зокрема створення макету галереї довільних зображень; закріпити навички роботи з делегуванням подій.</p>
+<p><strong>Місце розташування:</strong></p>
+<strong>Місце розташування:</strong> <a href="https://github.com/klynxlx/IS-32_appWEB-LinnykKatya-FIOT-2025-" target="_blank">посилання на репозиторій веб застосунка</a><br>
+        <a href="https://klynxlx.github.io/IS-32_appWEB-LinnykKatya-FIOT-2025-/" target="_blank">посилання на живу сторінку веб-застосунка</a><br>
+        <a href="https://github.com/klynxlx/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025" target="_blank">посилання на репозиторій самостійної роботи</a><br>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку самостійної роботи</a>
+        <a href="https://klynxlx.github.io/IS-32_INDEPENDENT-LinnykKatya-FIOT-2025/" target="_blank">посилання на живу сторінку звітного html-докумсенту</a>`
+;}
+
+function Task8_1() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+            <img src="./images/8_1.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/code8_1.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/code8_11.jpg" alt="Placeholder Image" width="700">`
+
+        ;
+    }
+function Task8_2() {
+    var outputDiv = document.getElementById("output");
+    outputDiv.innerHTML = `
+             <img src="./images/8_2.jpg" alt="Placeholder Image" width="700">
+            <img src="./images/code8_2.jpg" alt="Placeholder Image" width="700">`
+            
+        ;
+    }
+
+    function showConclusion8() {
+        var outputDiv = document.getElementById("output");
+        var conclusion = `
+            <p><strong>Висновок:</strong> Під час виконання ЛР №7 я придбала практичні навички роботистворення модального вікна; повторити процес створення макету, зокрема створення макету галереї довільних зображень; закріпити навички роботи з делегуванням подій.
+</p>`
+        ;
+        outputDiv.innerHTML = conclusion;
+    }  
